5d0457fa351859424450338d88bff6d3dcdf22a2
Timothy Crosley <timothy.crosley@gmail.com>
2019-05-16 03:36:38 -0700

    Attempt to build initial working implementation

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Personal Website Safe and Block List Management Script
 Requires `dnsmasq` installed and configured. To see how to setup:
 - https://computingforgeeks.com/install-and-configure-dnsmasq-on-ubuntu-18-04-lts/
 - https://www.linuxquestions.org/questions/linux-networking-3/how-to-block-all-websites-except-2-a-667350/
+
+
+For dynamic control:
+- sudo iptables -t nat -A OUTPUT -p tcp -d 1.1.1.1 -j DNAT --to-destination 127.0.0.1:7777
+- sudo sysctl -w net.ipv4.conf.all.route_localnet=1
---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+
+try:
+    from setuptools import setup
+    from setuptools.command.test import test as TestCommand
+
+    class PyTest(TestCommand):
+        extra_kwargs = {'tests_require': ['pytest', 'mock']}
+
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            import pytest
+            sys.exit(pytest.main(self.test_args))
+
+except ImportError:
+    from distutils.core import setup, Command
+
+    class PyTest(Command):
+        extra_kwargs = {}
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            raise SystemExit(subprocess.call([sys.executable, 'runtests.py']))
+
+try:
+   import pypandoc
+   readme = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError, OSError, RuntimeError):
+   readme = ''
+
+setup(name='allow',
+      version='0.0.1',
+      description="Personal Website Safe and Block List Management Script",
+      long_description=readme,
+      author='Timothy Crosley',
+      author_email='timothy.crosley@gmail.com',
+      url='https://github.com/timothycrosley/allow',
+      license='MIT',
+      requires=['hug'],
+      install_requires=['hug>=2.5.0', 'tldextract'],
+      entry_points={
+        'console_scripts': [
+            'allow = allow.run:allow.interface.cli',
+        ]
+      },
+      packages=['allow'],
+      keywords='Focus, Python, Python3, Firewall, Filter, Utility, Block, Allow',
+      **PyTest.extra_kwargs)
